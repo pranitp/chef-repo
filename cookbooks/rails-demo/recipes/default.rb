ed1dd179b48d69590fc64bba6d49892f68f9155a
pranitp <pranitpahune@gmail.com>
2014-12-20 21:40:04 +1100

    Pranit: Init cookbbook

--- cookbooks/rails-demo/recipes/default.rb
+++ cookbooks/rails-demo/recipes/default.rb
@@ -6,3 +6,14 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+application "rails-demo" do
+	path "/var/www/rails-apps/rails-demo"
+	owner "vagrant"
+	group "vagrant"
+	#Where to get this application
+	#repository "https://github.com/pranitp/chef-repo.git"
+	#repository "https://github.com/trotter/rails-hello-world.git"
+	#Sub resource
+	passenger_apache2	
+
+end
