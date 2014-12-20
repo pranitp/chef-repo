5eaa9a4128f32ef01132171e8782e2e3ec745edd
pranitp <pranitpahune@gmail.com>
2014-12-20 21:44:34 +1100

    Pranit: Init site cookbook

---
+++ cookbooks/java-install/attributes/default.rb
@@ -0,0 +1,5 @@
+override['java']['oracle']['accept_oracle_download_terms'] = true
+override['java']['accept_license_agreement'] = true
+ 
+default['java']['install_flavor'] = 'oracle'
+default['java']['jdk_version'] = '6'
